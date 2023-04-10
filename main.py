1628033d8852a258a0ecedcea3aee5bdfbff1f83
Sendiko <sarangtawon897@gmail.com>
2023-04-10 21:19:56 +0700

    added gui

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QComboBox, QTextEdit
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
+
+
 def convert_string_to_ascii(s):
     """Convert a string to its ASCII representation"""
     return [ord(c) for c in s]
@@ -18,27 +24,65 @@ def convert_decimal_to_octal(decimal):
     return oct(decimal)
 
 
-# Loop to repeat the program
-while True:
-    # Get user input
-    input_type = input("What would you like to convert? Enter 'string' or 'decimal', or 'exit' to quit: ")
+class ConverterGUI(QWidget):
 
-    if input_type == "string":
-        # Convert string to ASCII
-        s = input("Enter a string to convert to ASCII: ")
-        ascii_values = convert_string_to_ascii(s)
-        print("ASCII representation:", ascii_values)
-    elif input_type == "decimal":
-        # Convert decimal to binary, hexadecimal, and octal
-        decimal = int(input("Enter a decimal number to convert: "))
-        binary = convert_decimal_to_binary(decimal)
-        hexadecimal = convert_decimal_to_hexadecimal(decimal)
-        octal = convert_decimal_to_octal(decimal)
-        print("Binary representation:", binary)
-        print("Hexadecimal representation:", hexadecimal)
-        print("Octal representation:", octal)
-    elif input_type == "exit":
-        # Exit the program if the user enters 'exit'
-        break
-    else:
-        print("Invalid input type. Please enter 'string', 'decimal', or 'exit'.")
+    def __init__(self):
+        super().__init__()
+
+        # Set window properties
+        self.setWindowTitle("Number Converter")
+        self.setGeometry(100, 100, 600, 400)
+
+        # Create widgets
+        self.type_label = QLabel("Select type:", self)
+        self.type_label.setGeometry(50, 50, 100, 30)
+        self.type_combo = QComboBox(self)
+        self.type_combo.setGeometry(150, 50, 150, 30)
+        self.type_combo.addItem("String")
+        self.type_combo.addItem("Decimal")
+        self.type_combo.addItem("Exit")
+        self.input_label = QLabel("Input:", self)
+        self.input_label.setGeometry(50, 100, 100, 30)
+        self.input_box = QLineEdit(self)
+        self.input_box.setGeometry(150, 100, 150, 30)
+        self.output_label = QLabel("Output:", self)
+        self.output_label.setGeometry(50, 150, 100, 30)
+        self.output_box = QTextEdit(self)
+        self.output_box.setGeometry(150, 150, 400, 200)
+        self.convert_button = QPushButton("Convert", self)
+        self.convert_button.setGeometry(350, 100, 100, 30)
+
+        # Connect signal to slot
+        self.convert_button.clicked.connect(self.convert)
+
+        # Set font
+        font = QFont()
+        font.setPointSize(12)
+        self.type_label.setFont(font)
+        self.input_label.setFont(font)
+        self.output_label.setFont(font)
+
+    def convert(self):
+        input_type = self.type_combo.currentText().lower()
+        input_value = self.input_box.text()
+
+        if input_type == "string":
+            ascii_values = convert_string_to_ascii(input_value)
+            output_value = "ASCII representation: " + str(ascii_values)
+        elif input_type == "decimal":
+            decimal = int(input_value)
+            binary = convert_decimal_to_binary(decimal)
+            hexadecimal = convert_decimal_to_hexadecimal(decimal)
+            octal = convert_decimal_to_octal(decimal)
+            output_value = f"Binary representation: {binary}\nHexadecimal representation: {hexadecimal}\nOctal representation: {octal}"
+        elif input_type == "exit":
+            sys.exit()
+
+        self.output_box.setText(output_value)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    converter = ConverterGUI()
+    converter.show()
+    sys.exit(app.exec_())
